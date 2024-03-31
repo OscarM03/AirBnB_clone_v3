a90f9cffe2b74ecd25a705ba9eb611814697e9bb
OscarM03 <oscarmutwiri03@gmail.com>
2024-03-31 11:23:22 +0300

    Checking pycodestyle

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -21,7 +21,9 @@ def get_cities_in_states(state_id):
             cities.append(city.to_dict())
         return jsonify(cities)
 
-@app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
+
+@app_views.route('/cities/<city_id>',
+                 methods=['GET'], strict_slashes=False)
 def get_cities(city_id):
     """Get cities"""
     city = storage.get(City, city_id)
@@ -29,8 +31,10 @@ def get_cities(city_id):
         abort(404)
     else:
         return jsonify(city.to_dict())
-    
-@app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
+
+
+@app_views.route('/cities/<city_id>',
+                 methods=['DELETE'], strict_slashes=False)
 def delete_city(city_id):
     """Delete city"""
     city = storage.get(City, city_id)
@@ -39,15 +43,17 @@ def delete_city(city_id):
     else:
         storage.delete(city)
         storage.save()
-        return jsonify({})
-    
-@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+        return jsonify({}), 200
+
+
+@app_views.route('/states/<state_id>/cities',
+                 methods=['POST'], strict_slashes=False)
 def create_city(state_id):
     """creatre a new city"""
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
-    
+
     data = request.get_json()
     if not data:
         abort(400, "Not a JSON")
@@ -61,13 +67,14 @@ def create_city(state_id):
     return jsonify(new_obj.to_dict()), 201
 
 
-@app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/cities/<city_id>',
+                 methods=['PUT'], strict_slashes=False)
 def update_city(city_id):
     """update a city"""
     city = storage.get(City, city_id)
     if city is None:
         abort(404)
-    
+
     data = request.get_json()
     if not data:
         abort(400, "Not a JSON")
@@ -80,10 +87,3 @@ def update_city(city_id):
             setattr(city, key, value)
     storage.save()
     return jsonify(city.to_dict()), 200
-
-
-
-
-
-
-
